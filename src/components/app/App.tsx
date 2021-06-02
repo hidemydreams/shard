d9cac3b98ce380561df124f6fc854d5a5e179b4f
hidemydreams <naza.archakov222@gmail.com>
2021-06-03 02:42:03 +0300

    feat: add footer component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,14 +7,6 @@ const App: FC = () => {
     <div className="app">
       <Header />
       <SideBar />
-      <div className="text">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ullam nam
-        culpa voluptatum impedit. Accusantium eveniet facere pariatur sint natus
-        repudiandae ipsum earum assumenda nostrum suscipit cumque, libero
-        ratione eligendi. Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Quis ullam nam culpa voluptatum impedit. Accusantium eveniet
-      </div>
-      <footer></footer>
     </div>
   );
 };
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <div className="footer__column">
+          <div className="footer__contacts">
+            <a className="footer__tel" href="tel">
+              +7 (966) 065-55-66
+            </a>
+            <p className="footer__email">info@shard-studio.ru</p>
+          </div>
+          <p className="footer__cta">Стать клиентом</p>
+        </div>
+        <div className="footer__socials socials">
+          <nav className="socials">
+            <ul className="socials__list">
+              <li className="socials__link">Vkontakte</li>
+              <li className="socials__link">Instagram</li>
+              <li className="socials__link">Telegram</li>
+            </ul>
+            <div className="socials__copyright">
+              © Студия Шард 2020. Все права защищены.
+            </div>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
