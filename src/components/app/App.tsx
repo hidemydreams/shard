f3a442f87c40b302660e3f4e7b2af9c5bd4d9b9f
hidemydreams <naza.archakov222@gmail.com>
2021-06-03 02:01:24 +0300

    feat: add assets

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,57 +1,20 @@
 import React, { FC } from 'react';
 import Header from '../header/Header';
+import SideBar from '../side-bar/SideBar';
 import './App.scss';
-
 const App: FC = () => {
   return (
     <div className="app">
       <Header />
-      <aside className="sidebar">
-        <div className="sidebar__content">
-          <h1 className="sidebar__title">Услуги</h1>
-          <nav className="sidebar__services services__nav">
-            <p className="services__nav-active-link">WEB</p>
-            <p className="services__nav-next-link">Mobile</p>
-            <ul className="services__nav-list">
-              <li className="services__nav-link">Mobile</li>
-              <li className="services__nav-link">Design</li>
-              <li className="services__nav-link">Support</li>
-              <li className="services__nav-link">Branding</li>
-              <li className="services__nav-link">Marketing</li>
-            </ul>
-          </nav>
-        </div>
-        <nav className="service-nav">
-          <ul className="service-nav__list">
-            <li className="service-nav__link">
-              <p className="service-nav__link-number">01</p>
-              <div className="service-nav__link-text">Разработка лендинга</div>
-            </li>
-            <li className="service-nav__link service-nav__link--active">
-              <p className="service-nav__link-number">02</p>
-              <p className="service-nav__link-text">Разработка промосайта</p>
-            </li>
-            <li className="service-nav__link">
-              <p className="service-nav__link-number">03</p>
-              <div className="service-nav__link-text">
-                Разработка корпоративного сайта
-              </div>
-            </li>
-            <li className="service-nav__link">
-              <p className="service-nav__link-number">04</p>
-              <div className="service-nav__link-text">
-                Разработка интернет-магазина
-              </div>
-            </li>
-            <li className="service-nav__link">
-              <p className="service-nav__link-number">05</p>
-              <p className="service-nav__link-text">
-                Разработка интернет-портала
-              </p>
-            </li>
-          </ul>
-        </nav>
-      </aside>
+      <SideBar />
+      <div className="text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ullam nam
+        culpa voluptatum impedit. Accusantium eveniet facere pariatur sint natus
+        repudiandae ipsum earum assumenda nostrum suscipit cumque, libero
+        ratione eligendi. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Quis ullam nam culpa voluptatum impedit. Accusantium eveniet
+      </div>
+      <footer></footer>
     </div>
   );
 };
---
+++ src/components/side-bar/SideBar.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from 'react';
+import arrow from '../../assets/images/arrow.svg';
+import './SideBar.scss';
+
+const SideBar: FC = () => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__content">
+        <h1 className="sidebar__title">Услуги</h1>
+        <nav className="sidebar__services services-nav">
+          <div className="services-nav__row">
+            <p className="services-nav__active-link">WEB</p>
+            <p className="services-nav__next-link">
+              <img src={arrow} alt="Arrow Icon" /> Mobile
+            </p>
+          </div>
+          <ul className="services-nav__list">
+            <li className="services-nav__link">Mobile</li>
+            <li className="services-nav__link">Design</li>
+            <li className="services-nav__link">Support</li>
+            <li className="services-nav__link">Branding</li>
+            <li className="services-nav__link">Marketing</li>
+          </ul>
+        </nav>
+      </div>
+      <nav className="service-nav">
+        <ul className="service-nav__list">
+          <li className="service-nav__link">
+            <p className="service-nav__link-number">01</p>
+            <p className="service-nav__link-text">Разработка лендинга</p>
+            <div className="service-nav__link-line"></div>
+          </li>
+          <li className="service-nav__link service-nav__link--active">
+            <p className="service-nav__link-number">02</p>
+            <p className="service-nav__link-text">Разработка промосайта</p>
+            <div className="service-nav__link-line"></div>
+          </li>
+          <li className="service-nav__link">
+            <p className="service-nav__link-number">03</p>
+            <p className="service-nav__link-text">
+              Разработка корпоративного сайта
+            </p>
+            <div className="service-nav__link-line"></div>
+          </li>
+          <li className="service-nav__link">
+            <p className="service-nav__link-number">04</p>
+            <p className="service-nav__link-text">
+              Разработка интернет-магазина
+            </p>
+            <div className="service-nav__link-line"></div>
+          </li>
+          <li className="service-nav__link">
+            <p className="service-nav__link-number">05</p>
+            <p className="service-nav__link-text">
+              Разработка интернет-портала
+            </p>
+            <div className="service-nav__link-line"></div>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default SideBar;
