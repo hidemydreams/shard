02febe062b08a1f5d991bbe0d0ce5dea7ee02c45
hidemydreams <naza.archakov222@gmail.com>
2021-06-11 13:31:37 +0300

    feat: add sticky header

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import LandingDevelopment from '../../pages/LandingDevelopment/LandingDevelopment';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
@@ -6,13 +6,28 @@ import SideBar from '../side-bar/SideBar';
 
 import './App.scss';
 const App: FC = () => {
+  const mainContent = useRef<HTMLHeadingElement>(null);
+
+  const setStickyHeader = () => {
+    if (mainContent.current !== null) {
+      mainContent.current.classList.toggle(
+        'content-margin',
+        window.scrollY > 100
+      );
+    }
+  };
+  window.addEventListener('scroll', setStickyHeader);
   return (
     <div className="app">
       <div className="wrapper">
         <Header />
-        <SideBar />
-        <main className="main-content">
-          <LandingDevelopment />
+        <main ref={mainContent} className="main-content">
+          <div className="main-content__inner">
+            <SideBar />
+            <div className="main-content__page">
+              <LandingDevelopment />
+            </div>
+          </div>
         </main>
         <Footer />
       </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import logo from '../../assets/images/Logotype.svg';
 import './Header.scss';
 
 const Header: FC = () => {
+  const header = useRef<HTMLHeadingElement>(null);
+
+  const setStickyHeader = () => {
+    if (header.current !== null) {
+      header.current.classList.toggle('sticky', window.scrollY > 100);
+    }
+  };
+  window.addEventListener('scroll', setStickyHeader);
   return (
-    <header className="header">
-      <div className="header__logo logo">
-        <img className="logo__image" src={logo} alt="Shard Logo" />
-      </div>
-      <div className="header__right">
-        <p className="header__client-link">Стать клиентом</p>
-        <div className="header__burger burger-icon">
-          <div className="burger-icon__line"></div>
-          <div className="burger-icon__line"></div>
-          <div className="burger-icon__line"></div>
+    <header ref={header} className="header">
+      <div className="header__content">
+        <div className="header__logo logo">
+          <img className="logo__image" src={logo} alt="Shard Logo" />
+        </div>
+        <div className="header__right">
+          <p className="header__client-link">Стать клиентом</p>
+          <div className="header__burger burger-icon">
+            <div className="burger-icon__line"></div>
+            <div className="burger-icon__line"></div>
+            <div className="burger-icon__line"></div>
+          </div>
         </div>
       </div>
     </header>
